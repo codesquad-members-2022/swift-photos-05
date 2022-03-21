7940abb1c21153a59b2f1d1113bc90d59d2ac179
sanghee <anshee97@naver.com>
2022-03-21 11:58:30 +0900

    Feat: Add CollectionView Outlet

--- Photos/Photos/ViewController.swift
+++ Photos/Photos/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
