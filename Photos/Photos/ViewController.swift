ea673219997f9955035f708ec785b0148ccd7f6f
sanghee <anshee97@naver.com>
2022-03-21 11:48:28 +0900

    Feat: Add CollectionView

--- Photos/Photos/ViewController.swift
+++ Photos/Photos/ViewController.swift
@@ -11,9 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
+
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
