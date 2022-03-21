d14795198a294764bf01c292a8b80e93c154fc8b
sanghee <anshee97@naver.com>
2022-03-21 11:40:08 +0900

    Feat: 프로젝트 생성

---
+++ Photos/Photos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by 안상희 on 2022/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
