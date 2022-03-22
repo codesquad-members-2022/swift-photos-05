d0f5c637a9970b07491f3662cb63e3aaa9b0bb9e
P_SeoB <akdlsjtm21@naver.com>
2022-03-22 11:14:36 +0900

    Test: add UITest

---
+++ Photos/PhotosUITests/PhotosUITests.swift
@@ -0,0 +1,31 @@
+//
+//  PhotosUITests.swift
+//  PhotosUITests
+//
+//  Created by 박진섭 on 2022/03/22.
+//
+
+import XCTest
+
+class PhotosUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
