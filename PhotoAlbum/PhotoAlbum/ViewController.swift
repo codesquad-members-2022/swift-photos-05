ad7231d378dbbb46ed706196c71cf0b6c7bb4511
sanghee <anshee97@naver.com>
2022-03-22 14:57:38 +0900

    Remove: Delete Unnecessary codes Rename: ColorCollectionViewCell -> ImageCollectionViewCell

--- PhotoAlbum/PhotoAlbum/ViewController.swift
+++ PhotoAlbum/PhotoAlbum/ViewController.swift
@@ -5,17 +5,26 @@
 //  Created by 안상희 on 2022/03/21.
 //
 
+import Photos
 import UIKit
 
 class ViewController: UIViewController {
-
+    var allPhotos: PHFetchResult<PHAsset>?
+    let imageManager = PHCachingImageManager()
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.allPhotos = PHAsset.fetchAssets(with: nil) // 조건 (nil)에 맞는 모든 asset 가져오기
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        collectionView.reloadData() // 화면 전환할 때마다 데이터 reload
+    }
 }
 
 
@@ -27,8 +36,9 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFl
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = "colorCell"
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cell , for: indexPath) as? ColorCollectionViewCell else {
+        let asset = self.allPhotos?.object(at: indexPath.item)
+        let cellIdentifier = "imageCell"
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as? ImageCollectionViewCell else {
             return UICollectionViewCell()
         }
         return cell
