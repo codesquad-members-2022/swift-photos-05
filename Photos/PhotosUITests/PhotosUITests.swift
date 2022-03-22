8b5139a6d5a613583d68159aabb4dc8a2f405af1
P_SeoB <akdlsjtm21@naver.com>
2022-03-22 11:35:53 +0900

    Test: test navigationBar Title's Validation

--- Photos/PhotosUITests/PhotosUITests.swift
+++ Photos/PhotosUITests/PhotosUITests.swift
@@ -8,24 +8,23 @@
 import XCTest
 
 class PhotosUITests: XCTestCase {
-
+    
+    let app = XCUIApplication()
+    
     override func setUpWithError() throws {
+        try super.setUpWithError()
         continueAfterFailure = false
-    }
-
-    override func tearDownWithError() throws {
-    }
-
-    func testExample() throws {
-        let app = XCUIApplication()
         app.launch()
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    override func tearDownWithError() throws {
+        app.terminate()
+        try super.tearDownWithError()
     }
+
+    func testNavigationTitleIsValidate() throws {
+        //check navigation bar title
+        XCTAssertTrue(app.navigationBars.staticTexts["Photos"].exists)
+    }
+
 }
