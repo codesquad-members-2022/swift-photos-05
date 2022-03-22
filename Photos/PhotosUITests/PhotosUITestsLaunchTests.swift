fcae8d17c73665521b45d89380ac2ed5f219a115
P_SeoB <akdlsjtm21@naver.com>
2022-03-22 11:14:36 +0900

    Test: add UITest

---
+++ Photos/PhotosUITests/PhotosUITestsLaunchTests.swift
@@ -0,0 +1,29 @@
+//
+//  PhotosUITestsLaunchTests.swift
+//  PhotosUITests
+//
+//  Created by 박진섭 on 2022/03/22.
+//
+
+import XCTest
+
+class PhotosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
