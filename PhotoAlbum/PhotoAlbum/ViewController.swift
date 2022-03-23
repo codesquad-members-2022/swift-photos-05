2c825a30190f876bda44735c7af1eff33638fc51
P_SeoB <akdlsjtm21@naver.com>
2022-03-23 11:46:18 +0900

    fix: imageView contentMode

--- PhotoAlbum/PhotoAlbum/ViewController.swift
+++ PhotoAlbum/PhotoAlbum/ViewController.swift
@@ -28,15 +28,13 @@ final class ViewController: UIViewController {
         collectionView.reloadData() // 화면 전환할 때마다 데이터 reload
     }
     
-//    private func
-    
 }
 
 
 
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return allPhotos!.count
+        return allPhotos?.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -53,14 +51,15 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFl
         
         //이미지 캐싱.
         let cachingPhotos = self.allPhotos!.objects(at: IndexSet(integersIn: 0..<allPhotos!.count))
-        cacheManager.startCachingImages(for: cachingPhotos, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFill, options: nil)
+        cacheManager.startCachingImages(for: cachingPhotos, targetSize: CGSize(width: 100, height: 100), contentMode: .default, options: nil)
         
         // 이미지 불러오기
-        imageManger.requestImage(for: asset!, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFill, options: nil, resultHandler: {
+        imageManger.requestImage(for: asset!, targetSize: CGSize(width: 100, height: 100), contentMode: .default, options: nil, resultHandler: {
             // 이미지 불러오기 성공
             // 이전에 주입한 cell의 identifier와 asset의 identifier가 같은 경우, 이미지 넣기
             image, _ in if cell.representedIdentifier == asset?.localIdentifier {
                 cell.imageView.image = image
+                cell.imageView.contentMode = .scaleToFill
             }
         })
         return cell
