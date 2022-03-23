b9a71df9bb46c95dccf299fb020d203b62967e93
sanghee <anshee97@naver.com>
2022-03-23 15:35:51 +0900

    Feat: 이미지 캐시 기능 구현 Refactor: 피드백 리뷰 반영 (이미지 캐시 코드 분리)

--- PhotoAlbum/PhotoAlbum/ViewController.swift
+++ PhotoAlbum/PhotoAlbum/ViewController.swift
@@ -9,25 +9,35 @@ import Photos
 import UIKit
 
 final class ViewController: UIViewController {
-    private var allPhotos: PHFetchResult<PHAsset>? // 가져올 asset
-    private let cacheManager = PHCachingImageManager()
-    private let imageManger = PHImageManager()
-    
+    private var allPhotos: PHFetchResult<PHAsset>?
+    private let imageManager = PHCachingImageManager()
     
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.allPhotos = PHAsset.fetchAssets(with: nil) // 조건 (nil)에 맞는 모든 asset 가져오기
+        self.allPhotos = PHAsset.fetchAssets(with: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        collectionView.reloadData() // 화면 전환할 때마다 데이터 reload
+        collectionView.reloadData()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        configureCachingPhotos()
+    }
+    
+    private func configureCachingPhotos() {
+        let visibleCellCount = collectionView.visibleCells.count // 28
+        let cachingPhotos = self.allPhotos!.objects(at: IndexSet(integersIn: visibleCellCount - 10..<allPhotos!.count)) // 18..<50
+        imageManager.startCachingImages(for: cachingPhotos, targetSize: CGSize(width: 100, height: 100), contentMode: .default, options: nil)
+        imageManager.stopCachingImagesForAllAssets()
+    }
 }
 
 
@@ -38,25 +48,16 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFl
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        // allPhoto에서 indexPath에 해당하는 아이템을 가져와서 asset에 저장
         let asset = self.allPhotos?.object(at: indexPath.item)
         
         let cellIdentifier = "imageCell"
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as? ImageCollectionViewCell else {
             return UICollectionViewCell()
         }
-        
-        // asset의 고유 식별자를 cell의 식별자에 주입
+
         cell.representedIdentifier = asset?.localIdentifier
         
-        //이미지 캐싱.
-        let cachingPhotos = self.allPhotos!.objects(at: IndexSet(integersIn: 0..<allPhotos!.count))
-        cacheManager.startCachingImages(for: cachingPhotos, targetSize: CGSize(width: 100, height: 100), contentMode: .default, options: nil)
-        
-        // 이미지 불러오기
-        imageManger.requestImage(for: asset!, targetSize: CGSize(width: 100, height: 100), contentMode: .default, options: nil, resultHandler: {
-            // 이미지 불러오기 성공
-            // 이전에 주입한 cell의 identifier와 asset의 identifier가 같은 경우, 이미지 넣기
+        imageManager.requestImage(for: asset!, targetSize: CGSize(width: 100, height: 100), contentMode: .default, options: nil, resultHandler: {
             image, _ in if cell.representedIdentifier == asset?.localIdentifier {
                 cell.imageView.image = image
                 cell.imageView.contentMode = .scaleToFill
