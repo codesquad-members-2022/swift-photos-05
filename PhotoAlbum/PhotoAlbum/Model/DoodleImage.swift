ecd08698f06b6e0ff657bfd757a55c3e49ea4adc
sanghee <anshee97@naver.com>
2022-03-24 15:34:00 +0900

    Feat: Add Doodle Model

---
+++ PhotoAlbum/PhotoAlbum/Model/DoodleImage.swift
@@ -0,0 +1,21 @@
+//
+//  DoodleImage.swift
+//  PhotoAlbum
+//
+//  Created by 안상희 on 2022/03/24.
+//
+
+import Foundation
+
+struct DoodleImage: Codable {
+    let doodle: [Doodle]
+    var doodleCount: Int {
+        return self.doodle.count
+    }
+    
+    struct Doodle: Codable {
+        let title: String
+        let image: String
+        let date: String
+    }
+}
