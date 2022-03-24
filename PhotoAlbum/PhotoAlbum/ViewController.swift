7df1acdf0951026f2e8c57f9e0a460783493deb3
P_SeoB <akdlsjtm21@naver.com>
2022-03-25 01:01:58 +0900

    Refactor: move,insert,delete Each indexPath

--- PhotoAlbum/PhotoAlbum/ViewController.swift
+++ PhotoAlbum/PhotoAlbum/ViewController.swift
@@ -25,7 +25,7 @@ final class ViewController: UIViewController {
             default:
                 PHPhotoLibrary.shared().register(self)
         }
-        fetch()
+//        fetch()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -40,27 +40,26 @@ final class ViewController: UIViewController {
         configureCachingPhotos()
     }
     
-    private func fetch() {
-        guard let path = Bundle.main.path(forResource: "doodle", ofType: "json") else { return }
-        
-        guard let jsonString = try? String(contentsOfFile: path) else { return }
-        
-        let decoder = JSONDecoder()
-        let data = jsonString.data(using: .utf8)
-        
-        guard let data = data else { return }
-        
-        guard let myDoodles = try? decoder.decode([Doodle].self, from: data) else { return }
-        
-
-    }
+//    private func fetch() {
+//        guard let path = Bundle.main.path(forResource: "doodle", ofType: "json") else { return }
+//
+//        guard let jsonString = try? String(contentsOfFile: path) else { return }
+//
+//        let decoder = JSONDecoder()
+//        let data = jsonString.data(using: .utf8)
+//
+//        guard let data = data else { return }
+//
+//        guard let myDoodles = try? decoder.decode([Doodle].self, from: data) else { return }
+//
+//    }
     
     
     private func configureCachingPhotos() {
         let visibleCellCount = collectionView.visibleCells.count // 28
         let cachingPhotos = self.allPhotos!.objects(at: IndexSet(integersIn: visibleCellCount - 10..<allPhotos!.count)) // 18..<50
         imageManager.startCachingImages(for: cachingPhotos, targetSize: CGSize(width: 100, height: 100), contentMode: .default, options: nil)
-        imageManager.stopCachingImagesForAllAssets()
+        
     }
 }
 
@@ -72,20 +71,13 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFl
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let asset = self.allPhotos?.object(at: indexPath.item)
         
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCollectionViewCell.reuseIdentifier, for: indexPath) as? ImageCollectionViewCell else {
             return UICollectionViewCell()
         }
-
-        cell.representedIdentifier = asset?.localIdentifier
         
-        imageManager.requestImage(for: asset!, targetSize: CGSize(width: 100, height: 100), contentMode: .default, options: nil, resultHandler: {
-            image, _ in if cell.representedIdentifier == asset?.localIdentifier {
-                cell.imageView.image = image
-                cell.imageView.contentMode = .scaleToFill
-            }
-        })
+        inputCellImageWithPHAsset(cell: cell, at: indexPath)
+        
         return cell
     }
     
@@ -93,15 +85,64 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFl
         let length = 100
         return CGSize(width: length, height: length)
     }
+    
+    
+    private func inputCellImageWithPHAsset(cell:ImageCollectionViewCell,at indexPath:IndexPath) {
+        if let asset = self.allPhotos?.object(at: indexPath.item) {
+        
+        cell.representedIdentifier = asset.localIdentifier
+        
+        imageManager.requestImage(for: asset, targetSize: cell.frame.size, contentMode: .default, options: nil, resultHandler: {
+            image, _ in if cell.representedIdentifier == asset.localIdentifier {
+                cell.imageView.image = image
+                cell.imageView.contentMode = .scaleToFill
+                
+                    }
+                }
+            )
+        }
+    }
 }
 
 
+/*
+ 1.변화가 일어남.
+ 2.변화가 일어난 사진만 다시 Fetch해서 allPhotos에 대입.
+ 3.UI바꾸기.(변화에 따라 바뀐쪽만 UI업데이트 - main thread)
+*/
 extension ViewController: PHPhotoLibraryChangeObserver {
     func photoLibraryDidChange(_ changeInstance: PHChange) {
-        self.allPhotos = PHAsset.fetchAssets(with: nil)
-        
-        DispatchQueue.main.async {
-            self.collectionView.reloadData()
+        //옵셔널 강제추출은 가능... guard let은 왜 안가능?
+        DispatchQueue.main.sync {
+            var phassetCollection = PHAssetCollection.transientAssetCollection(withAssetFetchResult: allPhotos!, title: "PhotoResult")
+            
+            if let albumChanges = changeInstance.changeDetails(for: phassetCollection) {
+                phassetCollection = albumChanges.objectAfterChanges!
+            }
+            
+            if let changes = changeInstance.changeDetails(for: allPhotos!) {
+                allPhotos = changes.fetchResultAfterChanges
+                collectionView.performBatchUpdates({
+
+                    if let removed = changes.removedIndexes, removed.count > 0 {
+                        collectionView.deleteItems(at: removed.map { IndexPath(item: $0, section:0) })
+                    }
+                    if let inserted = changes.insertedIndexes, inserted.count > 0 {
+                        collectionView.insertItems(at: inserted.map { IndexPath(item: $0, section:0) })
+                    }
+                    if let changed = changes.changedIndexes, changed.count > 0 {
+                        collectionView.reloadItems(at: changed.map { IndexPath(item: $0, section:0) })
+                    }
+                    changes.enumerateMoves { fromIndex, toIndex in
+                        self.collectionView.moveItem(at: IndexPath(item: fromIndex, section: 0),
+                                                to: IndexPath(item: toIndex, section: 0))
+                    }
+                })
+            } else {
+                collectionView.reloadData()
+            }
         }
     }
+
+    
 }
