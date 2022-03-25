c250fa91bb53656b2de28876367fcf68089fea74
P_SeoB <akdlsjtm21@naver.com>
2022-03-25 16:57:42 +0900

    Feat: ADD DoodleCollecionViewCell & LoadIamge

--- PhotoAlbum/PhotoAlbum/Controller/DoodleViewController.swift
+++ PhotoAlbum/PhotoAlbum/Controller/DoodleViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class DoodleViewController: UICollectionViewController {
     
     let session = URLSession.shared
+    var doodleList = [UIImageView]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,16 +31,48 @@ class DoodleViewController: UICollectionViewController {
         
         guard let myDoodles = try? decoder.decode([Doodle].self, from: data) else { return }
         
-        for doodle in myDoodles {
-            guard let url = URL(string: doodle.image) else { return }
-            
+        for i in 0..<myDoodles.count {
+            guard let url = URL(string: myDoodles[i].image) else { return }
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.addValue("application/json", forHTTPHeaderField: "content-type")
+            session.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    print(error)
+                    return
+                }
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                    print((response as? HTTPURLResponse)?.statusCode)
+                    return
+                }
+                guard let data = data else {
+                    return
+                }
+                DispatchQueue.main.async { [weak self] in
+                    guard let self = self else { return }
+                    let imageView = UIImageView(image: UIImage(data: data))
+                    self.doodleList.append(imageView)
+                    self.collectionView.reloadData()
+                }
+                
+            }.resume()
         }
-        
-
     }
     
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return doodleList.count
+    }
     
-    
-    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DoodleCell", for: indexPath) as? DoodleCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        let image = doodleList[indexPath.item].image
+        DispatchQueue.main.async {
+            cell.imageView.image = image
+        }
+        return cell
+    }
     
 }
---
+++ PhotoAlbum/PhotoAlbum/View/DoodleCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DoodleCollectionViewCell.swift
+//  PhotoAlbum
+//
+//  Created by 박진섭 on 2022/03/25.
+//
+
+import UIKit
+
+class DoodleCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
