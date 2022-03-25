8659781ff1061af126564639a9a1de64fb487208
sanghee <anshee97@naver.com>
2022-03-25 15:22:01 +0900

    Feat: Change Storyboard to UICollectionViewController

--- PhotoAlbum/PhotoAlbum/Controller/DoodleViewController.swift
+++ PhotoAlbum/PhotoAlbum/Controller/DoodleViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DoodleViewController: UIViewController {
+class DoodleViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
