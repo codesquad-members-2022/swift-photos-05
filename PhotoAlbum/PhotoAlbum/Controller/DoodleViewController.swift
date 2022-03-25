3b11df6392d455df3994a39bbea543de331de416
sanghee <anshee97@naver.com>
2022-03-25 14:26:39 +0900

    Feat: Add NavigationController Programmatically

--- PhotoAlbum/PhotoAlbum/Controller/DoodleViewController.swift
+++ PhotoAlbum/PhotoAlbum/Controller/DoodleViewController.swift
@@ -7,20 +7,14 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
+class DoodleViewController: UIViewController {
 
-class DoodleViewController: UICollectionViewController {
-
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
-    }
-
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        return cell
     }
 }
--- PhotoAlbum/PhotoAlbum/SupportingFiles/SceneDelegate.swift
+++ PhotoAlbum/PhotoAlbum/SupportingFiles/SceneDelegate.swift
@@ -14,6 +14,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
     }
-
 }
 
