c71047fcffce63bf3e31cd797a85f08d7476a255
sanghee <anshee97@naver.com>
2022-03-25 11:26:26 +0900

    Refactor: photoLibraryDidChange(_:)

--- PhotoAlbum/PhotoAlbum/ViewController.swift
+++ PhotoAlbum/PhotoAlbum/ViewController.swift
@@ -25,7 +25,8 @@ final class ViewController: UIViewController {
             default:
                 PHPhotoLibrary.shared().register(self)
         }
-//        fetch()
+        
+        fetch()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -40,20 +41,18 @@ final class ViewController: UIViewController {
         configureCachingPhotos()
     }
     
-//    private func fetch() {
-//        guard let path = Bundle.main.path(forResource: "doodle", ofType: "json") else { return }
-//
-//        guard let jsonString = try? String(contentsOfFile: path) else { return }
-//
-//        let decoder = JSONDecoder()
-//        let data = jsonString.data(using: .utf8)
-//
-//        guard let data = data else { return }
-//
-//        guard let myDoodles = try? decoder.decode([Doodle].self, from: data) else { return }
-//
-//    }
-    
+    private func fetch() {
+        guard let path = Bundle.main.path(forResource: "doodle", ofType: "json") else { return }
+
+        guard let jsonString = try? String(contentsOfFile: path) else { return }
+
+        let decoder = JSONDecoder()
+        let data = jsonString.data(using: .utf8)
+
+        guard let data = data else { return }
+
+        guard let myDoodles = try? decoder.decode([Doodle].self, from: data) else { return }
+    }
     
     private func configureCachingPhotos() {
         let visibleCellCount = collectionView.visibleCells.count // 28
@@ -104,24 +103,12 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFl
     }
 }
 
-
-/*
- 1.변화가 일어남.
- 2.변화가 일어난 사진만 다시 Fetch해서 allPhotos에 대입.
- 3.UI바꾸기.(변화에 따라 바뀐쪽만 UI업데이트 - main thread)
-*/
 extension ViewController: PHPhotoLibraryChangeObserver {
     func photoLibraryDidChange(_ changeInstance: PHChange) {
-        //옵셔널 강제추출은 가능... guard let은 왜 안가능?
         DispatchQueue.main.sync {
-            var phassetCollection = PHAssetCollection.transientAssetCollection(withAssetFetchResult: allPhotos!, title: "PhotoResult")
-            
-            if let albumChanges = changeInstance.changeDetails(for: phassetCollection) {
-                phassetCollection = albumChanges.objectAfterChanges!
-            }
-            
-            if let changes = changeInstance.changeDetails(for: allPhotos!) {
-                allPhotos = changes.fetchResultAfterChanges
+            guard let allPhotos = allPhotos else { return }
+            if let changes = changeInstance.changeDetails(for: allPhotos) {
+                self.allPhotos = changes.fetchResultAfterChanges
                 collectionView.performBatchUpdates({
 
                     if let removed = changes.removedIndexes, removed.count > 0 {
@@ -133,16 +120,10 @@ extension ViewController: PHPhotoLibraryChangeObserver {
                     if let changed = changes.changedIndexes, changed.count > 0 {
                         collectionView.reloadItems(at: changed.map { IndexPath(item: $0, section:0) })
                     }
-                    changes.enumerateMoves { fromIndex, toIndex in
-                        self.collectionView.moveItem(at: IndexPath(item: fromIndex, section: 0),
-                                                to: IndexPath(item: toIndex, section: 0))
-                    }
                 })
             } else {
                 collectionView.reloadData()
             }
         }
     }
-
-    
 }
